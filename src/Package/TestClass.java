5b4f8693d0cf1327c8c2eebcf12e6a484ce7c32b
Yaewon Jeong <jenny102030@naver.com>
2022-04-06 00:14:14 +0900

    This is Commit Message!

---
+++ src/Package/TestClass.java
@@ -0,0 +1,11 @@
+package Package;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		System.out.println("Hello! This is Github Practicing!!");
+		// Github Practice!
+
+	}
+
+}
